352b46b9e51c1381aade2bf99e99426c64eb2fb4
ThanapatAe <47793299+ThanapatAe@users.noreply.github.com>
2019-09-29 19:34:03 +0700

    Add files via upload
    เพิ่ม CSS Class ชื่อ card ในไฟล์ App.css และกำหนด className ให้กับ div ของคอมโพเนนต์ CharacterCard

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import './App.css' ;
+
 
 export default class CharacterCard extends Component {
-    render(){
-        return (
-            <div>{this.props.value}</div>
-        )
+    constructor(props){
+    super(props)
+    this.state = {
+    active: false,
     }
-}
+    }
+   
+    activate = () => {
+        if(!this.state.active){
+        this.props.activationHandler(this.props.value)
+        this.setState({active: true})
+        }
+       }
+       
+
+    render(){
+    let className = `card ${this.state.active ? 'activeCard': ''}`
+    return (
+    <div className={className} onClick={this.activate}>
+    {this.props.value}
+    </div>
+    )
+    }
+   }
