69a98ecb5776370d4c0bbe899bcd61022d843f51
ThanapatAe <47793299+ThanapatAe@users.noreply.github.com>
2019-09-29 19:42:50 +0700

    สร้าง WordCard.js และแก้ไข CharacterCard
    แก้ไข CharacterCard เพื่อให้รองรับการถูก Click

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,29 +1,24 @@
 import React, { Component } from 'react';
-import './App.css' ;
-
-
 export default class CharacterCard extends Component {
-    constructor(props){
-    super(props)
-    this.state = {
-    active: false,
-    }
-    }
-   
+  render() {
+    return <div className = "card">{this.props.value}</div>;
+  }
+
+    constructor(props){ 
+        super(props) 
+        this.state = { active: false, } 
+    } 
+
     activate = () => {
         if(!this.state.active){
-        this.props.activationHandler(this.props.value)
-        this.setState({active: true})
-        }
-       }
-       
-
-    render(){
-    let className = `card ${this.state.active ? 'activeCard': ''}`
-    return (
-    <div className={className} onClick={this.activate}>
-    {this.props.value}
-    </div>
-    )
+            this.setState({active: true}) } 
     }
-   }
+
+    render(){ let className = `card ${this.state.active ? 'activeCard': ''}` 
+        return ( 
+            <div className={className} onClick={this.activate}> 
+                {this.props.value} 
+            </div> 
+        ) 
+    } 
+}
